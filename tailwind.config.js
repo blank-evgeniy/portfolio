5e6e104cdafa399b562d75c55a8925c4b45e7e8b
Evgeniy Blank <blank.eugeny@yandex.ru>
2024-08-08 19:22:17 +0200

    Added main font and colors to html and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                main: ['Advent Pro', 'sans-serif'],
+            },
+            colors: {
+                main: '#0a0a0a',
+                secondary: '#a33327',
+                accent: '#5CDB95',
+            },
+        },
     },
     plugins: [],
 };
